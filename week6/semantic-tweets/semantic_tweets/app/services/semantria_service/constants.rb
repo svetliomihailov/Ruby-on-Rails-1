d98a282a80518f905f165f15e5c605e092da6327
Svetlozar Mihailov <svetliomihailov@gmail.com>
2015-05-25 18:23:51 +0300

    Week 6 task - Done

---
+++ week6/semantic-tweets/semantic_tweets/app/services/semantria_service/constants.rb
@@ -0,0 +1,8 @@
+module SemantriaService
+  module Constants
+    USERNAME = 'stonedteddybear'
+    API_KEY = '59335674-6036-4a87-af82-ca6672f44e67'
+    API_SECRET = 'bf7bf906-ba5b-47b9-95a6-829055a27979'
+    APPLICATION_NAME = 'Semantic tweets'
+  end
+end
---
+++ week6/semantic-tweets/semantic_tweets/app/services/twitter_service/constants.rb
@@ -0,0 +1,9 @@
+module TwitterService
+  module Constants
+    USERNAME = 'hackrails'
+    TOKEN = '3233708379-rwLC41307Lw6eiLyzduLpSONbtnOn01ejqlTtVu'
+    TOKEN_SECRET = '4HlNXHXcuxKA03FGEy0uWbRr2EQH38rAcTllwzRLAL1KT'
+    API_KEY = 'IlMQjLkxdEJdfCSKXTYZxxLGA'
+    API_SECRET = '6F5N7TYScVrLOprHMaBm7dRSyTYW3GuDI4r021fCe9smurtdgN'
+  end
+end
